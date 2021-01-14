0652b6cf64389f6730c73ca98330944f428103df
Seoyoung2 <ww0111@naver.com>
2021-01-14 17:17:01 +0900

    SWEA: first java algo

---
+++ src/SWEA/홀수만더하기_2072.java
@@ -0,0 +1,23 @@
+package SWEA;
+
+import java.util.Scanner;
+
+public class 홀수만더하기_2072 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		int n, sum;
+		for (int i = 1; i <= T; i++) {
+			sum = 0;
+			for(int j = 0; j < 10; j++) {
+				n = sc.nextInt();
+				if (n % 2 == 1) {
+					sum += n;
+				}
+			}
+			System.out.println("#" + i +" " + sum);
+		}
+	}
+
+}
