383456ed5d6e64a5e9db8e97a952e8a60ea29a4a
Seoyoung2 <ww0111@naver.com>
2021-02-21 16:52:37 +0900

    fix: boj1325 시간초과 해결

--- src/Baekjoon/Main_1325.java
+++ src/Baekjoon/Main_1325.java
@@ -1,64 +1,62 @@
 package Baekjoon;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-
-// 1325. 효율적인 해킹
+//1325. 효율적인 해킹
 public class Main_1325 {
-	static int N, M;
-	static LinkedList<Integer> depend[];;
-	static boolean[] check;
-	static int[] hack;
 	
+	static int[] hack;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
 		
-		depend = new LinkedList[N+1];
-		for (int i = 1; i <= N; i++) {
-			depend[i] = new LinkedList<>();
-		}
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 		
-		int A, B;
+		ArrayList<Integer>[] depend = new ArrayList[N+1];
+		for (int i = 1; i <= N; i++)	depend[i] = new ArrayList<>();
+		
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			A = Integer.parseInt(st.nextToken());
-			B = Integer.parseInt(st.nextToken());
-			depend[A].add(B);	// A가 B를 신뢰한다
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			depend[a].add(b);
 		}
-
-		// dfs
+		
 		hack = new int[N+1];
 		for (int i = 1; i <= N; i++) {
-			check = new boolean[N+1];
-			dfs(i);
+			boolean[] visited = new boolean[N + 1];
+			dfs(i, visited, depend);
 		}
 
 		int max = 0;
 		for (int i = 1; i <= N; i++) {
 			max = Math.max(max, hack[i]);
 		}
+		
 		for (int i = 1; i <= N; i++) {
-			if(hack[i] == max) {
-				System.out.print(i+" ");
-			}
+			if (hack[i] == max)	bw.write(i + " ");
 		}
+		bw.flush();
+		bw.close();
 	}
 	
-	static void dfs(int idx) {
-		check[idx] = true;
-		for (int i=0, end=depend[idx].size(); i < end; i++) {
-			int tmp = depend[idx].get(i);
-			if(!check[tmp]) {
+	public static void dfs(int idx, boolean[] visited, ArrayList<Integer>[] depend) {
+		visited[idx] = true;
+		for (int tmp : depend[idx]) {
+			if (!visited[tmp]) {
 				hack[tmp]++;
-				dfs(tmp);
+				dfs(tmp, visited, depend);
 			}
 		}
 	}
-} 
+
+}
