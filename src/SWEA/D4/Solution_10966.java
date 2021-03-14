a4e6be51f8c3a70a6894ed322a394d17fca2c22c
Seoyoung2 <ww0111@naver.com>
2021-03-14 23:32:19 +0900

    boj, swea: 0314 algo

--- src/SWEA/D4/Solution_10966.java
+++ src/SWEA/D4/Solution_10966.java
@@ -3,12 +3,8 @@ package SWEA.D4;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 // 10966. 물놀이를 가자
