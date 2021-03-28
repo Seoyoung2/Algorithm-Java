6297d7a9a8e9a97c32d38879f772efccdef14d1e
Seoyoung2 <ww0111@naver.com>
2021-03-28 14:08:19 +0900

    fix: fix Main template

--- src/Baekjoon/Main_.java
+++ src/Baekjoon/Main_.java
@@ -5,7 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+// 0000. 
+public class Main_ {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
